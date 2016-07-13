a505eef799419256bb3038c5de60170aa77f1f3a
Thomas Durand <Dean151@users.noreply.github.com>
2016-07-13 11:17:00 +0200

    ScrollToPrevious/NextSection functions now calls delegate

--- Sources/Classes/UserInteractionFunctions.swift
+++ Sources/Classes/UserInteractionFunctions.swift
@@ -225,7 +225,7 @@ extension JTAppleCalendarView {
     public func scrollToNextSegment(triggerScrollToDateDelegate: Bool = false, animateScroll: Bool = true, completionHandler:(()->Void)? = nil) {
         let page = currentSectionPage + 1
         if page < monthInfo.count {
-            scrollToSection(page,  animateScroll: animateScroll, completionHandler: completionHandler)
+            scrollToSection(page, triggerScrollToDateDelegate: triggerScrollToDateDelegate, animateScroll: animateScroll, completionHandler: completionHandler)
         }
     }
     /// Scrolls the calendar view to the previous section view. It will execute a completion handler at the end of scroll animation if provided.
@@ -234,7 +234,7 @@ extension JTAppleCalendarView {
     public func scrollToPreviousSegment(triggerScrollToDateDelegate: Bool = false, animateScroll: Bool = true, completionHandler:(()->Void)? = nil) {
         let page = currentSectionPage - 1
         if page > -1 {
-            scrollToSection(page, animateScroll: animateScroll, completionHandler: completionHandler)
+            scrollToSection(page, triggerScrollToDateDelegate: triggerScrollToDateDelegate, animateScroll: animateScroll, completionHandler: completionHandler)
         }
     }
 
