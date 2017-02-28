8dd38bc025c3733d1f4b7c4d90325bab69c44d72
JayT <patchthecode@gmail.com>
2017-02-28 11:37:00 -0800

    Bump version after fix

--- Sources/GlobalFunctionsAndExtensions.swift
+++ Sources/GlobalFunctionsAndExtensions.swift
@@ -23,11 +23,11 @@ extension Calendar {
     func endOfMonth(for date: Date) -> Date? {
         guard
             let comp = dateFormatterComponents(from: date),
-            let day = self.range(of: .day, in: .month, for: date)?.count else {
+            let day = self.range(of: .day, in: .month, for: date)?.count,
+            let retVal = Calendar.formatter.date(from: "\(comp.year) \(comp.month) \(day)") else {
                 return nil
         }
-        
-        return Calendar.formatter.date(from: "\(comp.year) \(comp.month) \(day)")
+        return retVal
     }
     
     private func dateFormatterComponents(from date: Date) -> (month: Int, year: Int)? {
