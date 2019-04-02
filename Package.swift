47cb1e9e17f7fd3841c7d30f5745109b9192dd98
Simon Blum <Simon.Blum@jamitlabs.com>
2019-04-02 11:29:47 +0200

    Preparations for usage with Accio (https://github.com/JamitLabs/Accio)

--- Package.swift
+++ Package.swift
@@ -1,6 +1,17 @@
+// swift-tools-version:4.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
 import PackageDescription
 
-
 let package = Package(
-    name: "JTAppleCalendar"
+    name: "JTAppleCalendar",
+    products: [
+       .library(name: "JTAppleCalendar", targets: ["JTAppleCalendar"])
+   ],
+   targets: [
+       .target(
+           name: "JTAppleCalendar",
+           path: "Sources"
+       )
+   ]
 )
